d9a5e1e6c3d719c053d73240809518127c3a0bd7
Elena Romanowska <romanowska.elena@gmail.com>
2024-06-16 00:06:10 +0200

    navbar added+router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import './App.scss'
-import Categories from './components/Сategories/Categories.jsx'
+import { Link, Route, Routes } from 'react-router-dom'
+
+import MainPage from './pages/MainPage.jsx' 
+import CategoriesPage from './pages/CategoriesPage.jsx' 
+import AllProductsPage from './pages/AllProductsPage.jsx' 
+import AllSalesPage from './pages/AllSalesPage.jsx' 
+import Error404 from './pages/Error404.jsx' 
+import Cart from './pages/Cart.jsx' 
 
 
 const App = () => {
   return (
-    <div>
-      <Categories/>
-    </div>
+    <Routes>
+
+          <Route index element={<MainPage />} />
+          {/* <Route path='/' element={<MainPage />} /> */}
+          <Route path='/categories' element={<CategoriesPage />} />
+          <Route path='/allproducts' element={<AllProductsPage />} />
+          <Route path='/allsales' element={<AllSalesPage />} />
+          <Route path='*' element={<Error404 />} />
+          <Route path='/cart' element={<Cart />} />
+
+     
+    </Routes> 
   )
 }
 
 export default App
+
---
+++ src/pages/AllProductsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllProductsPage = () => {
+  return (
+    <div>AllProductsPage</div>
+  )
+}
+
+export default AllProductsPage
