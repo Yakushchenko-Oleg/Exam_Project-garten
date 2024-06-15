b54719a952f5450b19a04ef785a9ec16c2b40b94
alona <aloyna.vasilieva@gmail.com>
2024-06-15 20:15:40 +0200

    Add component DiscountForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css'
+import DiscountForm from './components/DiscountForm/DiscountForm'
 
 const App = () => {
   return (
     <div>
-      Exam project
+     <DiscountForm />
     </div>
   )
 }
--- src/components/DiscontForm/DiscontForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DiscontForm = () => {
-  return (
-    <div>DiscontForm</div>
-  )
-}
-
-export default DiscontForm
