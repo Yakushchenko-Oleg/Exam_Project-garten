dfe0633b7a78be13bc0579997d01d89e2fcb2821
Yakushchenko-Oleg <111yov111@gmail.com>
2024-06-19 15:54:03 +0200

    added components sales and single product. singleproduct doesn`t finished

--- src/components/Sales/Sales.jsx
+++ src/components/Sales/Sales.jsx
@@ -1,11 +1,17 @@
 import React, { useEffect } from 'react'
+import '../../App.scss';
+import "./Sales.scss";
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchAllProducts } from '../../store/productSlice'
+import SingleProduct from '../SingleProduct/SingleProduct'
+import { Link } from 'react-router-dom'
 
 
 const Sales = () => {
 
-const {products} = useSelector(state => state.products)
+const {products, isLoading, error} = useSelector(state => state.products)
+
+const discoutProducts = products.filter(item => item.discont_price)
 
 const dispatch = useDispatch()
 
@@ -15,20 +21,29 @@ function randomIndexInArray(array) {
    return Math.floor(Math.random() * array.length)
 }
 
-// console.log(products);
-// console.log(randomIndexInArray(products));
-
 return (
-    <div>
-        {/* <SingleProduct product={products[randomIndexInArray(products)]}/>
-        <SingleProduct product={products[randomIndexInArray(products)]}/>
-        <SingleProduct product={products[randomIndexInArray(products)]}/>
-        <SingleProduct product={products[randomIndexInArray(products)]}/>
-        <div>
-            <h2>{product.title}</h2>
 
+  <div className="sales">
+      <div className="sales__header-wrapper">
+        <h2>Sale</h2>
+        <div className="sales__header-wrapper_line"></div>
+        <Link to="/allsales">
+          <span className="sales__link">All sales</span>
+        </Link>
+      </div>
+
+      {isLoading ? (
+        <div className="loader"></div>
+      ) : (
+        <div className="sales__wrapper">
+          {/* <SingleProduct product={discoutProducts[randomIndexInArray(discoutProducts)]}/>
+          <SingleProduct product={discoutProducts[randomIndexInArray(discoutProducts)]}/>
+          <SingleProduct product={discoutProducts[randomIndexInArray(discoutProducts)]}/>
+          <SingleProduct product={discoutProducts[randomIndexInArray(discoutProducts)]}/> */}
         </div>
-       */}
+      )}
+      {error && <h2> Error from server: {error} </h2>}
     </div>
-
+  )
+}
 export default Sales
