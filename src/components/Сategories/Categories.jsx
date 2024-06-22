89fe8f878398a10aec706218d5a402f4e25694d9
Elena Romanowska <romanowska.elena@gmail.com>
2024-06-22 13:55:43 +0200

    categoriesItem-013 done

--- src/components/Сategories/Categories.jsx
+++ src/components/Сategories/Categories.jsx
@@ -4,7 +4,7 @@ import "./Categories.scss";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAllCategoties} from "../../store/categoriesProductsSlice";
-import CategoriesItem from "./CategoriesItem";
+import CategoriesItem from "../CategoriesItem/CategoriesItem";
 
 // добавить полоску к all categories
 
