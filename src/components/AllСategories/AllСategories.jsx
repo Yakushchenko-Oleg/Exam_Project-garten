b5a82eb06899545c21c4b308f9ab45f9ed68b001
Oleg Yakushchenko <137884563+Yakushchenko-Oleg@users.noreply.github.com>
2024-06-28 12:11:03 +0200

    Revert "Alena 2.01"

--- src/components/AllСategories/AllСategories.jsx
+++ src/components/AllСategories/AllСategories.jsx
@@ -1,15 +1,21 @@
-// src/components/AllCategories/AllCategories.jsx
 import React, { useEffect } from "react";
+
+import "./AllСategories.scss";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchAllCategoties } from "../../store/categoriesProductsSlice";
+import { fetchAllCategoties} from "../../store/categoriesProductsSlice";
 import CategoriesItem from "../CategoriesItem/CategoriesItem";
-import Navigation from "../Navigation/Navigation";
-import "./AllСategories.scss";
 
-const AllCategories = () => {
-  const { categories, isLoading, error } = useSelector((state) => state.categories);
+// добавить полоску к all categories
+
+const AllСategories = () => {
+  const { categories, isLoading, error } = useSelector(
+    (state) => state.categories
+  );
+
+
   const apiUrl = import.meta.env.APP_API_URL;
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -17,28 +23,33 @@ const AllCategories = () => {
   }, [dispatch]);
 
   return (
-    <div className="AllCategories">
-      <Navigation currentPage="Categories" />
-      <div className="AllCategories__header-wrapper">
-        <h2>Categories</h2>
+    <div className="AllСategories">
+      <div className="AllСategories__header-wrapper">
+        <h2>Сategories</h2>
       </div>
       
-      {isLoading ? (
-        <div className="loader"></div>
-      ) : (
-        <div className="AllCategories__wrapper">
-          {categories.map(item => (
-            <Link to={`/categories/${item.id}`} className="item__link" key={item.id}>
-              <CategoriesItem item={item} apiUrl={apiUrl} />
-            </Link>
-          ))}
-        </div>
-      )}
+        {
+          isLoading ? (
+            <div className="loader"></div>
+          ) : ( 
+            <div className="AllСategories__wrapper">
+            {
+              categories.map( item => (
+                <Link to={`/categories/${item.id}`} className='item__title' key={item.id}>
+                    <CategoriesItem item={item} apiUrl={apiUrl} key={item.id} />
+                </Link>
+
+                
+              ))
+            }
+            </div>
+          )
+        }
+        {error && <h2> Error from server: {error} </h2>}
       
-      {error && <h2>Error from server: {error}</h2>}
     </div>
   );
 };
 
-export default AllCategories;
 
+export default AllСategories;
