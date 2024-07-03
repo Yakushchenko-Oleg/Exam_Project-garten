5807039d86cae6faaf6c565308d5547ef24775c1
Yakushchenko-Oleg <111yov111@gmail.com>
2024-07-03 16:18:19 +0200

    added component Cart and CartItem without adaptiv

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,13 +1,47 @@
 import React from 'react'
+import './CartItem.scss'
 
 const CartItem = ({product}) => {
-  return (
-    <div>
-        <span>
-            <h3>{product.title}</h3>
-            <h5>{product.price}</h5>           
-        </span>
+  const apiUrl = import.meta.env.APP_API_URL;
 
+  return (
+
+    <div className='cartitem'>
+        <div className="cartitem__image">
+            <img src={`${apiUrl}${product.image}`} alt={product.title} />
+        </div>
+
+        <div className="cartitem__info">
+      
+            <div className="cartitem__info_title-container">  
+             <span>{product.title}</span>  
+             <button>X</button> 
+            </div>
+
+            <div className="cartitem__info__price-wraper">
+
+              <div className="cartitem__info__price-wraper_quantity-container">
+                <button>+</button>
+                <p>{product.quantity}</p> 
+                <button>-</button>
+              </div>
+
+              <div className="cartitem__info__price-wraper_price-container">
+                {
+                product.discont_price ? (
+                <>
+                  <span className="discount-price">{`$${product.discont_price}`}</span>
+                  <span className="original-price">{`$${product.price}`}</span>
+                </>
+                  ) : (
+                    <span className="info-price">{`$${product.price}`}</span>
+                  )}
+              </div>
+
+            </div>
+
+        </div>
+        
     </div>
   )
 }
