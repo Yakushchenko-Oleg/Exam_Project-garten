9d45085502829c04403b185f4fc3d7fd601853ae
Yakushchenko-Oleg <111yov111@gmail.com>
2024-07-08 12:41:02 +0200

    changed icons in single prosuct abd styles

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  envPrefix: 'APP_'
+  plugins: [react(), svgr()],
+  envPrefix: 'APP_',
+
 })
