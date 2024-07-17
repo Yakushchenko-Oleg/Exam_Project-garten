521d0e5f708478ea50c341a2fb76c6050d48299a
Elena Romanowska <romanowska.elena@gmail.com>
2024-07-17 15:16:10 +0200

    theme

--- src/providers/ThemeProvider.jsx
+++ src/providers/ThemeProvider.jsx
@@ -1,24 +1,23 @@
 import React, {createContext, useEffect} from 'react'
 import { useState } from 'react';
-// import { useLocalStorage } from '../hooks/useLocalStorage'
+
 
 export const ThemeContext = createContext();
 
 const ThemeContextProvider = ({ children }) => {
-  const [theme, setTheme] = useState(() => {
-    
+    const [theme, setTheme] = useState(() => {
     const storedTheme = localStorage.getItem('theme');
     return storedTheme ? JSON.parse(storedTheme) : false;
   });
 
   useEffect(() => {
-     localStorage.setItem('theme', JSON.stringify(theme));
+    localStorage.setItem('theme', JSON.stringify(theme));
   }, [theme]);
 
-
-  const toggleTheme = () => {
+    const toggleTheme = () => {
     setTheme((prevTheme) => !prevTheme);
   };
+
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
