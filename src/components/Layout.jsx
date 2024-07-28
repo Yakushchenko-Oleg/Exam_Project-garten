8a44edfbb037e4c692c967453d9a22dccbef271d
Elena Romanowska <romanowska.elena@gmail.com>
2024-07-28 16:20:59 +0200

    file styles corrected

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,17 +5,19 @@ import { Outlet } from 'react-router-dom'
 import { ThemeContext } from '../providers/ThemeProvider'
 
 const Layout = () => {
-  
-  const {theme} = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext);
 
   return (
-
-    <div className={ `container-1440 ${theme ? 'container-1440-dark' : 'container-1440-light'}`}>
-      <NavBar/>
-        <Outlet/>
-      <Contacts/> 
+    <div
+      className={`container-1440 ${
+        theme ? "container-1440-dark" : "container-1440-light"
+      }`}
+    >
+      <NavBar />
+      <Outlet />
+      <Contacts />
     </div>
-  )
-}
+  );
+};
 
 export default Layout
